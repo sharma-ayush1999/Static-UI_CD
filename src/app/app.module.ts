893468eb02c92241e0462a6cbe037245c4cb9d78
sharma-ayush1999 <sharma.ayush3103@gmail.com>
2020-12-06 12:16:07 +0530

    College list json added and college list component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CollegeListComponent } from './college-list/college-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CollegeListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/college-list/college-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-college-list',
+  templateUrl: './college-list.component.html',
+  styleUrls: ['./college-list.component.scss']
+})
+export class CollegeListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
